59a32b96d54b14d51356ecc8013873f9f9d73327
Ray-Al-Amin <alraiman2k15@hotmail.com>
2020-11-05 09:52:30 +0100

    Add project files.

---
+++ BDD Prüfungsaufgabe/Trigonometrie.feature
@@ -0,0 +1,18 @@
+﻿Feature: Trigonometrie
+	Trigonometrie
+
+@mytag
+Scenario: Sinus
+    Given the first number is Pi
+    When sinus is taken
+    Then the result should be 0
+
+Scenario: Cosinus
+    Given the first number is Pi
+    When cosinus is taken
+    Then the result should be -1
+
+Scenario: Tangens
+    Given the first number is Pi
+    When tangens is taken
+    Then the result should be 0
---
+++ BDD Prüfungsaufgabe/TrigonometrieSteps.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace BDD_Prüfungsaufgabe
+{
+    [Binding]
+    public class TrigonometrieSteps
+    {
+        double number;
+        [Given(@"the first number is Pi")]
+        public void GivenTheFirstNumberIsPi()
+        {
+            number = Math.PI;
+        }
+        
+        [When(@"sinus is taken")]
+        public void WhenSinusIsTaken()
+        {
+            number = Math.Sin(number);
+        }
+        
+        [When(@"cosinus is taken")]
+        public void WhenCosinusIsTaken()
+        {
+            number = Math.Cos(number);
+        }
+        
+        [When(@"tangens is taken")]
+        public void WhenTangensIsTaken()
+        {
+            number = Math.Tan(number);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            Assert.Equal(p0, number, 5);
+        }
+    }
+}
